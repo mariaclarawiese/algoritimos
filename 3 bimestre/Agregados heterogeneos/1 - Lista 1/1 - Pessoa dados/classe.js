2d55fd64faa8f9a025357ee7dd6f76170097a215
Maria Clara <mariaclara@MacBook-Air-de-Igor.local>
2026-09-15 15:40:56 -0300

    oi

---
+++ 3 bimestre/Agregados heterogeneos/1 - Lista 1/1 - Pessoa dados/classe.js
@@ -0,0 +1,8 @@
+class Pessoa {
+    constructor (CPF, nome, nascimento, altura) {
+        this.CPF = CPF;
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.altura = altura;
+    }
+}
---
+++ 3 bimestre/Agregados heterogeneos/1 - Lista 1/2 - Leitura /livros.js
@@ -0,0 +1,9 @@
+class Livro {
+    constructor (ISBN, Titulo, Autor, AnoPublic, Preco) {
+        this.ISBN = ISBN;
+        this.Titulo = Titulo;
+        this.Autor = Autor;
+        this.AnoPublic = AnoPublic;
+        this.Preco = Preco;
+    }
+}
